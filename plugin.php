d91b741367370bdfef88d9e7aec623a32a038bec
studioleland <lelandzaremba@gmail.com>
2015-06-01 16:58:02 -0400

    Added stylesheet includes

--- plugin.php
+++ plugin.php
@@ -1,14 +1,18 @@
 <?php
 /**
  * @wordpress-plugin
- * Plugin Name:       MyPlugin
- * Plugin URI:        http://plugin-name.com/
+ * Plugin Name:       WooCreative Bridge CSS
+ * Plugin URI:        http://woo-creative.com/
  * Description:       A plugin.
  * Version:           1.0.0
- * Author:            Author
- * Author URI:        http://author.com/
+ * Author:            Leland Zaremba
+ * Author URI:        http://woo-creative.com/
  * License:           MIT
- */
+**/
  
- 
- ?>
+add_action( 'wp_enqueue_scripts', 'woo_add_stylesheet' );
+
+function woo_add_stylesheet() {
+  wp_enqueue_style( 'prefix-style', plugins_url('style.css', __FILE__) );
+}
+?>
