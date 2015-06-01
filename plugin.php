f8441724d53629688028416e8fe95afa21a3849b
studioleland <lelandzaremba@gmail.com>
2015-06-01 16:46:46 -0400

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @wordpress-plugin
+ * Plugin Name:       MyPlugin
+ * Plugin URI:        http://plugin-name.com/
+ * Description:       A plugin.
+ * Version:           1.0.0
+ * Author:            Author
+ * Author URI:        http://author.com/
+ * License:           MIT
+ */
+ 
+ 
+ ?>
